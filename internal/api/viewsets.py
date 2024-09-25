938e5774d7d0ee90c6d6993464ba9923c442c087
romashorodok <108732029+romashorodok@users.noreply.github.com>
2024-09-25 17:59:48 +0300

    feat: query all models and serialize them

--- internal/api/viewsets.py
+++ internal/api/viewsets.py
@@ -1,15 +1,19 @@
 import re
 import keyword
+import json
 
 from http import HTTPMethod
 from typing import Any
 
 from django.db import models
 from django.db.backends.base.base import BaseDatabaseWrapper
+from django.db.backends.base.introspection import FieldInfo
+from django.db.backends.utils import CursorWrapper
 from django.db.utils import load_backend
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.serializers import ModelSerializer
 from rest_framework.viewsets import ModelViewSet, ViewSet
 
 from database_query_builder_api.settings import TIME_ZONE
@@ -191,6 +195,215 @@ class QueryMetaClass(type):
         return super().__new__(cls, name, bases, attrs)
 
 
+FIELD_MAPPING_CLASS = {
+    "CharField": models.CharField,
+    "IntegerField": models.IntegerField,
+    "BooleanField": models.BooleanField,
+    "DateTimeField": models.DateTimeField,
+    "ForeignKey": models.ForeignKey,
+    "AutoField": models.AutoField,
+    "BigAutoField": models.BigAutoField,
+    "TextField": models.TextField,
+}
+
+
+class DatabaseScanner:
+    def __init__(self, db: BaseDatabaseWrapper, cursor: CursorWrapper) -> None:
+        self.__db = db
+        self.__cursor = cursor
+
+        self.__model_classes = dict[str, type[models.Model]]()
+        self.__model_serializer_classess = dict[str, type[ModelSerializer]]()
+
+    def get_models(self):
+        return self.__model_classes
+
+    def get_model_serializer_classess(self):
+        return self.__model_serializer_classess
+
+    def __get_table_primary_key_column_name(self, table: str) -> str | None:
+        primary_key_columns = self.__db.introspection.get_primary_key_columns(
+            self.__cursor, table
+        )
+        return primary_key_columns[0] if primary_key_columns else None
+
+    def __get_table_relations(self, table: str) -> dict:
+        try:
+            relations = self.__db.introspection.get_relations(self.__cursor, table)
+        except NotImplementedError:
+            relations = {}
+        return relations
+
+    def __get_unique_columns(self, table: str):
+        try:
+            constraints = self.__db.introspection.get_constraints(self.__cursor, table)
+        except NotImplementedError:
+            constraints = {}
+        return [
+            c["columns"][0]
+            for c in constraints.values()
+            if c["unique"] and len(c["columns"]) == 1
+        ]
+
+    def __create_drf_serializer_cls(
+        self, model_cls: type[models.Model], fields_to_serialize: list[str]
+    ) -> type[ModelSerializer]:
+        class Meta:
+            model = model_cls
+            fields = fields_to_serialize
+
+        serializer_fields = {}
+        serializer_fields["__module__"] = ""
+        serializer_fields["Meta"] = Meta
+
+        return type(
+            f"{str(model_cls)}Serializer", (ModelSerializer,), serializer_fields
+        )
+
+    def scan_all_tables(self):
+        types = {
+            "t",  # Tables
+            "p",  # Partitions
+            "v",  # Views
+        }
+        table_info = self.__db.introspection.get_table_list(self.__cursor)
+        table_info = {info.name: info for info in table_info if info.type in types}
+
+        for table in sorted(name for name in table_info):
+            primary_key_column = self.__get_table_primary_key_column_name(table)
+            unique_columns = self.__get_unique_columns(table)
+
+            model_name = normalize_table_name(table)
+
+            relations = self.__get_table_relations(table)
+            # Holds column names used in the table so far
+            used_column_names = []
+            # Maps column names to names of model fields
+            column_to_field_name = {}
+            # Holds foreign relations used in the table.
+            used_relations = set()
+
+            known_models = [model_name]
+            fields: dict[str, Any] = {}
+
+            for row in self.__db.introspection.get_table_description(
+                self.__cursor,
+                table,
+            ):
+                row: FieldInfo = row
+                column_name = row.name
+                is_relation = column_name in relations
+
+                # Holds Field parameters such as 'db_column'.
+                extra_params = {}
+
+                att_name, params, _ = normalize_col_name(
+                    column_name, used_column_names, is_relation
+                )
+
+                extra_params.update(params)
+                used_column_names.append(att_name)
+
+                column_to_field_name[column_name] = att_name
+
+                if column_name == primary_key_column:
+                    extra_params["primary_key"] = True
+                elif column_name in unique_columns:
+                    extra_params["unique"] = True
+
+                if is_relation:
+                    ref_db_column, ref_db_table = relations[column_name]
+                    if extra_params.pop("unique", False) or extra_params.get(
+                        "primary_key"
+                    ):
+                        rel_type = "OneToOneField"
+                    else:
+                        rel_type = "ForeignKey"
+                        ref_pk_column = self.__db.introspection.get_primary_key_column(
+                            self.__cursor, ref_db_table
+                        )
+                        if ref_pk_column and ref_pk_column != ref_db_column:
+                            extra_params["to_field"] = ref_db_column
+                    rel_to = (
+                        "self"
+                        if ref_db_table == table
+                        else normalize_table_name(ref_db_table)
+                    )
+                    if rel_to in known_models:
+                        field_type = "%s(%s" % (rel_type, rel_to)
+                    else:
+                        field_type = "%s('%s'" % (rel_type, rel_to)
+
+                    if rel_to in used_relations:
+                        extra_params["related_name"] = "%s_%s_set" % (
+                            model_name.lower(),
+                            att_name,
+                        )
+
+                    used_relations.add(rel_to)
+                    print(
+                        "relation",
+                        rel_to,
+                        "ref table",
+                        ref_db_table,
+                        "ref on",
+                        ref_db_column,
+                        "local_column",
+                        column_name,
+                        "rel",
+                        rel_type,
+                        rel_to,
+                    )
+                    # TODO: add foregin key type
+
+                else:
+                    field_type, field_params, _ = get_field_type(self.__db, table, row)
+                    extra_params.update(field_params)
+
+                # TODO: field construct on own func
+                field = FIELD_MAPPING_CLASS.get(field_type)
+                if not field:
+                    # print(
+                    #     "Not found field mapper",
+                    #     model_name,
+                    #     column_name,
+                    #     field_type,
+                    #     extra_params,
+                    # )
+                    continue
+
+                fields[column_name] = field(**extra_params)
+
+                # print(model_name, column_name, field_type, extra_params)
+
+            # print(fields)
+
+            fields_to_serialize = list(map(lambda q: q[0], fields.items()))
+
+            fields["__module__"] = ""
+
+            class Meta:
+                db_table = table
+                managed = False
+                app_label = ""
+
+            fields["Meta"] = Meta
+            django_model_class: type[models.Model] = type(
+                model_name, (models.Model,), fields
+            )
+
+            django_drf_model_serializer = self.__create_drf_serializer_cls(
+                django_model_class, fields_to_serialize
+            )
+
+            self.__model_serializer_classess[model_name] = django_drf_model_serializer
+            self.__model_classes[model_name] = django_model_class
+
+            # print(django_model_class)
+            # print("result of", django_model_class.objects.all())
+        pass
+
+
 class QueryViewSet(ViewSet):
     @action(methods=[HTTPMethod.GET], detail=False)
     def select(self, _, data_source: str):
@@ -228,210 +441,25 @@ class QueryViewSet(ViewSet):
         db.settings_dict["OPTIONS"] = {}
 
         with db.cursor() as cursor:
-            types = {
-                "t",  # Tables
-                "p",  # Partitions
-                "v",  # Views
-            }
-            table_info = db.introspection.get_table_list(cursor)
-            table_info = {info.name: info for info in table_info if info.type in types}
+            scanner = DatabaseScanner(db, cursor)
+            scanner.scan_all_tables()
 
-            for table_name in sorted(name for name in table_info):
-                try:
-                    relations = db.introspection.get_relations(cursor, table_name)
-                except NotImplementedError:
-                    relations = {}
+            data = []
+            serializers = scanner.get_model_serializer_classess()
+            print("Found N model serializers:", len(serializers))
 
-                primary_key_columns = db.introspection.get_primary_key_columns(
-                    cursor, table_name
-                )
-                primary_key_column = (
-                    primary_key_columns[0] if primary_key_columns else None
-                )
+            for model, cls in scanner.get_models().items():
+                serializer = serializers.get(model)
+                if not serializer:
+                    print(f"Not found {model} model serializer")
+                    continue
 
-                try:
-                    constraints = db.introspection.get_constraints(cursor, table_name)
-                except NotImplementedError:
-                    constraints = {}
-                unique_columns = [
-                    c["columns"][0]
-                    for c in constraints.values()
-                    if c["unique"] and len(c["columns"]) == 1
-                ]
+                result = cls.objects.all()
 
-                table_description = db.introspection.get_table_description(
-                    cursor, table_name
-                )
+                s = serializer(result, many=True)
 
-                model_name = normalize_table_name(table_name)
-                print("class %s(models.Model):" % model_name)
-
-                known_models = [model_name]
-                used_column_names = []  # Holds column names used in the table so far
-                column_to_field_name = {}  # Maps column names to names of model fields
-                used_relations = set()  # Holds foreign relations used in the table.
-
-                for row in table_description:
-                    comment_notes = []  # Holds Field notes, to be displayed in a Python comment.
-                    extra_params = {}  # Holds Field parameters such as 'db_column'.
-                    column_name = row.name
-                    is_relation = column_name in relations
-
-                    att_name, params, notes = normalize_col_name(
-                        column_name, used_column_names, is_relation
-                    )
-                    extra_params.update(params)
-                    comment_notes.extend(notes)
-
-                    used_column_names.append(att_name)
-                    column_to_field_name[column_name] = att_name
-
-                    if column_name == primary_key_column:
-                        extra_params["primary_key"] = True
-                        if primary_key_columns and len(primary_key_columns) > 1:
-                            comment_notes.append(
-                                "The composite primary key (%s) found, that is not "
-                                "supported. The first column is selected."
-                                % ", ".join(primary_key_columns)
-                            )
-                    elif column_name in unique_columns:
-                        extra_params["unique"] = True
-
-                    if is_relation:
-                        ref_db_column, ref_db_table = relations[column_name]
-                        if extra_params.pop("unique", False) or extra_params.get(
-                            "primary_key"
-                        ):
-                            rel_type = "OneToOneField"
-                        else:
-                            rel_type = "ForeignKey"
-                            ref_pk_column = db.introspection.get_primary_key_column(
-                                cursor, ref_db_table
-                            )
-                            if ref_pk_column and ref_pk_column != ref_db_column:
-                                extra_params["to_field"] = ref_db_column
-                        rel_to = (
-                            "self"
-                            if ref_db_table == table_name
-                            else normalize_table_name(ref_db_table)
-                        )
-                        if rel_to in known_models:
-                            field_type = "%s(%s" % (rel_type, rel_to)
-                        else:
-                            field_type = "%s('%s'" % (rel_type, rel_to)
-                        if rel_to in used_relations:
-                            extra_params["related_name"] = "%s_%s_set" % (
-                                model_name.lower(),
-                                att_name,
-                            )
-                        used_relations.add(rel_to)
-                    else:
-                        # Calling `get_field_type` to get the field type string and any
-                        # additional parameters and notes.
-                        field_type, field_params, field_notes = get_field_type(
-                            db, table_name, row
-                        )
-                        extra_params.update(field_params)
-                        comment_notes.extend(field_notes)
-
-                        field_type += "("
-
-                    if att_name == "id" and extra_params == {"primary_key": True}:
-                        if field_type == "AutoField(":
-                            continue
-                        elif (
-                            field_type
-                            == db.features.introspected_field_types["AutoField"] + "("
-                        ):
-                            comment_notes.append("AutoField?")
-
-                    # Add 'null' and 'blank', if the 'null_ok' flag was present in the
-                    # table description.
-                    if row.null_ok:  # If it's NULL...
-                        extra_params["blank"] = True
-                        extra_params["null"] = True
-
-                    field_desc = "%s = %s%s" % (
-                        att_name,
-                        # Custom fields will have a dotted path
-                        "" if "." in field_type else "models.",
-                        field_type,
-                    )
-                    if field_type.startswith(("ForeignKey(", "OneToOneField(")):
-                        field_desc += ", models.DO_NOTHING"
-
-                    if db.features.supports_comments and row.comment:
-                        extra_params["db_comment"] = row.comment
-
-                    if extra_params:
-                        if not field_desc.endswith("("):
-                            field_desc += ", "
-                        field_desc += ", ".join(
-                            "%s=%r" % (k, v) for k, v in extra_params.items()
-                        )
-                    field_desc += ")"
-                    if comment_notes:
-                        field_desc += "  # " + " ".join(comment_notes)
-
-                    print("field desc:", "    %s" % field_desc)
-
-                    comment = None
-
-                    if info := table_info.get(table_name):
-                        is_view = info.type == "v"
-                        is_partition = info.type == "p"
-                        if db.features.supports_comments:
-                            comment = info.comment
-                    else:
-                        is_view = False
-                        is_partition = False
-
-                    meta = get_meta(
-                        table_name,
-                        constraints,
-                        column_to_field_name,
-                        is_view,
-                        is_partition,
-                        comment,
-                    )
-                    # print(meta)
-
-                    fields = {}
-
-                    # Add some default fields if necessary
-                    fields["id"] = models.AutoField(primary_key=True)
-
-                    # Define the Meta class if needed (this is optional)
-                    if meta:
-
-                        class Meta:
-                            db_table = table_name
-                            managed = False
-                            app_label = ""
-
-                        fields["Meta"] = Meta
-
-                    fields["__module__"] = ""
-
-                    django_model_class = type(
-                        model_name,
-                        (models.Model,),
-                        fields,
-                    )
-                    model: models.Model = django_model_class
-                    print("model class", model)
-
-                    dynamic_result = model.objects.all()
-                    print(
-                        "result from",
-                        django_model_class,
-                        dynamic_result,
-                    )
+                data.extend(s.data)
 
         db.close()
 
-        data = [
-            {"id": 1, "name": "Item 1", "description": "Description 1"},
-            {"id": 2, "name": "Item 2", "description": "Description 2"},
-        ]
         return Response(data, status=status.HTTP_200_OK)
